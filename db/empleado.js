f2416d56d34d206a3b811b7ba99b096dcc229a59
HeronSamperio <joseheronsamperio@gmail.com>
2021-04-05 16:09:37 -0500

    agrega los modelos de empleado y sus hijas, tienda y prospecto

---
+++ db/empleado.js
@@ -0,0 +1,50 @@
+const { BOOLEAN } = require("sequelize");
+
+module.export=(DB,type) => {
+    return DB.define('empleado', {
+        idEmpleado: {
+             type: type.INTEGER,
+             primaryKey:true,
+             autoIncrement:true,
+         },
+        nombre: {
+             type: type.STRING,
+             noEmpty:true,
+         }, 
+        apellidoPaterno :{
+             type:type. STRING,
+             noEmpty:true,
+         },
+        apellidoMaterno:{
+            type: type.STRING,
+            noEmpty:true,
+         },
+        correoElectronico: {
+             type:type.STRING,
+             noEmpty:true,
+             unique:true,
+             validate:{
+                 isEmail:true,
+             }
+         },
+        numTelefono:{
+            type: type.STING,
+            noEmpty:true,
+            unique:true,
+        },
+        puesto: {
+            type:type.STRING,
+            noEmpty:true,
+        },
+        contrasena: {
+            type:type.STRING,
+            noEmpty:true,
+        },
+        activo:{
+            type: type.BOOLEAN,
+            default: false
+        },
+     },{
+         paranoid:true
+     });
+}
