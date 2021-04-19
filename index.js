a555ca410849fcc5f8a8baecfb77c36fe64e8bdb
Diana Aguirre <a01276380@itesm.mx>
2021-04-19 02:35:14 -0500

    Se separa el código propio de la base de datos y del servidor de Express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+const employees = require('./employees')
+
+app.use(bodyParser.json());
+
+// Para evitar el horroroso error de CORS
+app.use(cors());
+app.use('/employees', employees)
+
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`)
+}) 
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    return res.status(500).json({
+        "name":err.name,
+        "message": `${err.message}, ${err.original ? err.original : ':('}`,
+    })
+})
