ddfa46daf7ca4271e056584b338cb7e8dead1e8b
Diana Aguirre <a01276380@itesm.mx>
2021-04-21 19:26:21 -0500

    Router del modelo Contacts

---
+++ contacts.js
@@ -0,0 +1,28 @@
+// Importamos Express, el Router y los modelos necesarios para las queries
+const express = require('express');
+const router = express.Router();
+const { Prospect, Contact } = require('./database');
+
+// Endpoint que creará nuevas instancias de contactos asociados a un prospecto
+router.post('/', async(req, res, next) => {
+    
+    const { idProspect, idContact, compromiso } = req.body
+    const contacto = { idContact, idProspect, compromiso }
+
+    try {
+        let prospecto = await Prospect.findByPk(idProspect)
+        if(prospecto) {
+            await Contact.create(contacto)
+            return res.status(201).json({contacto})
+        } else {
+            return res.status(404).json({
+                name: "Not Found",
+                message: "El prospecto al que haces referencia no existe :("
+            })
+        }
+    } catch {
+        next(err)
+    }
+})
+
+module.exports = router
